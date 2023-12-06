88408c1dbd6d1474a1b63edd3d126516589755f4
GarryCan76 <95485005+GarryCan76@users.noreply.github.com>
2023-12-06 14:22:01 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio</title>
+    <link rel="stylesheet" href="css/home.css">
+</head>
+<body>
+<div class="container">
+    <div class="welcome">
+        <h1>welcome to my portfolio</h1>
+    </div>
+    <div class="about-me">
+        <h2>about me</h2>
+        <div class="split">
+            <div>
+                <h3>Who am I</h3>
+                <p>Lorem ipsum dolor sit amet. Qui maxime labore et perspiciatis optio At blanditiis iure non voluptas dolores eum fuga excepturi cum obcaecati soluta aut dicta minima. Rem aliquid illum aut dignissimos iste non perferendis omnis 33 sint praesentium.
+                    Eos error itaque ad aspernatur consequatur ea aliquam molestiae et saepe mollitia est asperiores recusandae. Et perferendis sint sit nemo molestias ut nemo fugit eum Quis eveniet.
+                </p>
+                <h3>Goals and future</h3>
+                <p>Lorem ipsum dolor sit amet. Qui maxime labore et perspiciatis optio At blanditiis iure non voluptas dolores eum fuga excepturi cum obcaecati soluta aut dicta minima. Rem aliquid illum aut dignissimos iste non perferendis omnis 33 sint praesentium.
+                    Eos error itaque ad aspernatur consequatur ea aliquam molestiae et saepe mollitia est asperiores recusandae. Et perferendis sint sit nemo molestias ut nemo fugit eum Quis eveniet.
+
+            </div>
+            <div>
+                <h3>Picture</h3>
+                <img src="https://picsum.photos/200/300" alt="">
+            </div>
+        </div>
+    </div>
+    <div class="testimonials">
+        <h2>testimonials</h2>
+        div
+    </div>
+    <div class="my-projects">
+        <h2>my projects</h2>
+    </div>
+    <div class="contact-me">
+        <h2>contact me</h2>
+    </div>
+</div>
+</body>
+</html>
